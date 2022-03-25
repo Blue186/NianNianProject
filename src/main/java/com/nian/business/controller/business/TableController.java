84ae15472fce667a36f06efbd94fc89ee95dc46e
CommonRom <commonrom@qq.com>
2022-03-25 13:22:38 +0800

    完成Table系列接口

--- src/main/java/com/nian/business/controller/business/TableController.java
+++ src/main/java/com/nian/business/controller/business/TableController.java
@@ -4,14 +4,14 @@ import cn.hutool.json.JSONObject;
 import com.nian.business.entity.Business;
 import com.nian.business.entity.Table;
 import com.nian.business.entity.vo.table.TableIDNamePeopleNum;
+import com.nian.business.entity.vo.table.TableNamePeopleNum;
 import com.nian.business.service.TableService;
 import com.nian.business.utils.R;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +20,6 @@ import java.util.List;
 public class TableController {
     @Resource
     private TableService tableService;
-
 
     @GetMapping
     public R<?> getTables(HttpServletRequest request) {
@@ -43,4 +42,61 @@ public class TableController {
         return R.ok().message("成功").detail(jsonObject);
     }
 
+
+    @PostMapping
+    public R<?> createTable(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestBody TableNamePeopleNum rJson
+    ){
+        Business business = (Business) request.getAttribute("business");
+
+        Table table = new Table();
+        table.setBusinessId(business.getId());
+        table.setName(rJson.getName());
+        table.setPeopleNums(rJson.getPeopleNums());
+
+        Integer ret = tableService.createTable(table);
+        if (ret != 1){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return R.error().message("创建失败");
+        }
+
+        return R.ok().message("success").detail(null);
+    }
+
+    @DeleteMapping("/{tableID}")
+    public R<?> deleteTable(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @PathVariable Integer tableID
+    ){
+        Business business = (Business) request.getAttribute("business");
+
+        Integer ret = tableService.deleteTable(business.getId(), tableID);
+        if (ret != 1){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return R.error().message("删除失败");
+        }
+
+        return R.ok().status("success").message("delete table success");
+    }
+
+    @PutMapping("/{tableID}")
+    public R<?> updateTable(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @PathVariable Integer tableID,
+            @RequestBody TableNamePeopleNum rJson
+    ){
+        Business business = (Business) request.getAttribute("business");
+
+        Integer ret = tableService.updateTable(business.getId(), tableID, rJson.getName(), rJson.getPeopleNums());
+        if (ret != 1){
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            return R.error().message("更新失败");
+        }
+
+        return R.ok().message("update table success");
+    }
 }
--- src/main/java/com/nian/business/service/impl/TableServiceImpl.java
+++ src/main/java/com/nian/business/service/impl/TableServiceImpl.java
@@ -2,23 +2,48 @@ package com.nian.business.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.nian.business.entity.Business;
 import com.nian.business.entity.Table;
-import com.nian.business.mapper.CategoryMapper;
 import com.nian.business.mapper.TableMapper;
 import com.nian.business.service.TableService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class TableServiceImpl extends ServiceImpl<TableMapper, Table> implements TableService {
 
     @Override
     public List<Table> selectAll(Integer businessID) {
-        QueryWrapper<Table> wrapper = new QueryWrapper<>();
-        wrapper.eq("business_id", businessID);
-        return baseMapper.selectList(wrapper);
+        return baseMapper.selectList(new QueryWrapper<Table>().eq("business_id", businessID));
+    }
+
+    @Override
+    public Integer createTable(Table table) {
+        return baseMapper.insert(table);
+    }
+
+    @Override
+    public Integer deleteTable(Integer businessID, Integer tableID) {
+        Map<String , Object> map = new HashMap<>();
+        map.put("business_id" , businessID);
+        map.put("id" , tableID);
+
+        return baseMapper.delete(new QueryWrapper<Table>().allEq(map));
+    }
+
+    @Override
+    public Integer updateTable(Integer businessID, Integer tableID, String name, Integer peopleNums) {
+        Table table = new Table();
+        table.setName(name);
+        table.setPeopleNums(peopleNums);
+
+        Map<String , Object> map = new HashMap<>();
+        map.put("business_id" , businessID);
+        map.put("id" , tableID);
+
+        return baseMapper.update(table, new QueryWrapper<Table>().allEq(map));
     }
 }
